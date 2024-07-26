f12e5ef02df6dd561677167203c46a61d65a7488
naufinur <khayatinaufi23gmail.com>
2024-07-26 11:16:24 +0700

    ⚙:fix cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://besthy-fe.vercel.app",
   })
 );
 app.use(
@@ -34,7 +34,7 @@ app.use("/message", messageRoute);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://besthy-fe.vercel.app",
   },
 });
 
