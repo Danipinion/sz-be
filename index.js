d9ec6dfc1e1ca96e1d44c0a45eadc85d43e47001
naufinur <khayatinaufi23gmail.com>
2024-07-27 20:27:49 +0700

    ⚙: fix cors

--- index.js
+++ index.js
@@ -6,12 +6,7 @@ import messageRoute from "./routes/message.route.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:5173",
-  })
-);
+app.use(cors());
 
 app.use("/message", messageRoute);
 
